cf3db5e135fcd197783e7162c1666aeab626419c
Erb IT-Services <erbitservices@gmail.com>
2024-08-04 16:16:52 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,7 @@ import NavBar from "../components/NavBar";
 function Home() {
   return (
     <>
-      <BrowserRouter>
-        <NavBar></NavBar>
-        <Routes>
-          <Route path="/"></Route>
-          <Route path="/Dataintable" element={<Dataintable />}></Route>
-          <Route path="/SingleData" element={<SingleData />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Dataintable></Dataintable>
       {/* <Dataintable></Dataintable> */}
     </>
   );
