5a202b5fd0192c263ff353297a8d7ad46095b19f
ErbITservices <erbitservices@gmail.com>
2024-08-04 13:31:57 +0530

    j

--- src/pages/Dataintable.jsx
+++ src/pages/Dataintable.jsx
@@ -2,14 +2,38 @@ import Enter from "../components/Enter";
 import Input from "../components/input";
 import { useState } from "react";
 import { useEffect } from "react";
+import axios from 'axios'
 function Dataintable() {
+  const [url,seturl] = useState("");
+
   let [arr, datastate] = useState([]);
-  const [data, setData] = useState([]);
+  let [data, setData] = useState([]);
+
+  const showdata = (e) => {
+    
+      let city = e.target.value;
+      seturl(city);
+      
+      seturl((state)=>{
+        console.log(state + " mihir");
+        
+        return state;
+      })
+      console.log("m");
+      
+      
+        datastate(data.filter((elm)=>elm.gam===city));
+        console.log();
+        
+  };
+  
+ 
   useEffect(() => {
     const dataget = async () => {
       try {
         const response = await axios.get(`http://localhost:3333/form/`);
-        console.log(response.data), setData(response.data);
+        console.log(response.data), 
+        setData(response.data);
       } catch (error) {
         console.log(error);
       }
@@ -78,7 +102,7 @@ function Dataintable() {
   ];
   let category = ["city", "town-1", "town-2", "village"];
   let [library, state] = useState([]);
-  let city = ["unjha", "visnagar", "meshana", "gandhinagar", "bhavnagar"];
+  let city = ["unjha", "ગામ:2", "meshana", "gandhinagar", "bhavnagar"];
   let town_1 = ["rajkot", "sidhpur", "baroda"];
   let town_2 = ["palanpur", "deesa", "dhanera"];
   let village = ["upera", "jaska", "sundhiya"];
@@ -100,18 +124,7 @@ function Dataintable() {
     }
     console.log(event.target.value);
   };
-  const showdata = (e) => {
-    console.log(e.target.value);
-    if (e.target.value == "unjha") {
-      datastate(data);
-    } else if (e.target.value == "sidhpur") {
-      datastate(sidhpur);
-    } else if (e.target.value == "palanpur") {
-      datastate(palanpur);
-    } else {
-      datastate([]);
-    }
-  };
+  
   return (
     <>
       <Input
@@ -120,7 +133,7 @@ function Dataintable() {
         handleclick={handleonclick}
         showdata={showdata}
       ></Input>
-      {data.length != 0 && <Enter arr={data}></Enter>}
+      {arr.length != 0 && <Enter arr={arr}></Enter>}
     </>
   );
 }
