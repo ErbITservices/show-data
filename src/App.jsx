730fcb41921d3cac36b83206cf46a2868b9fc7e4
Erb IT-Services <erbitservices@gmail.com>
2024-08-06 11:22:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,22 @@ import Input from "./components/input";
 import Home from "./pages/Home";
 import Dataintable from "./pages/Dataintable";
 import Login from "./pages/login";
+import NavBar from "./components/NavBar";
+import SingleData from "./pages/SinglData";
+
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 
 function App() {
   return (
     <center>
-      {/* <Login></Login> */}
-      <Home></Home>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/Home" element={<Home />}></Route>
+          <Route path="/Dataintable" element={<Dataintable />}></Route>
+          <Route path="/SingleData" element={<SingleData />}></Route>
+        </Routes>
+      </BrowserRouter>
     </center>
   );
 }
